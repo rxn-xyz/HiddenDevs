46271c6be010e6a961ac8c845b8120809eb9d247
rxn-xyz <122142128+rxn-xyz@users.noreply.github.com>
2024-06-30 19:00:23 -0700

    Create DefuserExplained.lua

---
+++ DefuserExplained.lua
@@ -0,0 +1,336 @@
+-- defuser is a puzzel solver for the game defusal (https://www.roblox.com/games/3577061261/Defuse)
+
+local defuser = {}
+
+defuser.operations = {
+	--[[
+		these functions allow me to use operation variables within if statements without loadstring
+
+		e.g.
+			concept:
+				local operation = <
+				local x, y = 1, 2
+
+				print(x operation y) --> true
+
+			execution:
+				local operation = "<"
+				local x, y = 1, 2
+
+				print(defuser.operations[operation](x, y)) --> true
+	]]
+
+	["+"]  = function(x, y) return x + y end,
+	["-"]  = function(x, y) return x - y end,
+	["*"]  = function(x, y) return x * y end,
+	["/"]  = function(x, y) return x / y end,
+	[">"]  = function(x, y) return x > y end,
+	["<"]  = function(x, y) return x < y end,
+	[">="] = function(x, y) return x >= y end,
+	["<="] = function(x, y) return x <= y end,
+	["=="] = function(x, y) return x == y end
+}
+
+
+function defuser.interval(interval, number)
+	--[[
+		function that determines if a number is within an interval
+
+		quick explanation of intervals
+
+			"[]" are closed brackets and includes the number they're next to
+			"()" are open and exlude the number they're next to
+
+			[1, 5] = 1-5
+			(1, 5) = 2-4
+			(1, 5] = 2-5
+			[1, 5) = 1-4
+		
+		this function handles the intervals above but also another type
+
+			e.g.
+				"> 1" = (1, inf)
+				">= 1" = [1, inf]
+				"< 1" = (inf, 1)
+				"<= 1" = (inf, 1]
+	]]
+
+	if interval:find("[><=]=?") then
+		--[[
+			checks if its the [operator][number] interval
+
+				"[]" = either or, so "[><=]" looks for either >, <, or =
+				"?" = isnt required, so =? checks if theres a "=" or not
+				
+				"[><=]=?" will find the following strings
+
+					">", "<", "=", ">=", "<=", "==" (all of our operators excluding "=")
+
+			if its that type of interval it then breaks it down and gets the operator and the number
+			
+			it works with rational numbers by checking if theres a decimal ("[.]") after the first set of numbers ("%d+") and then checks for numbers after the decimal ("%d*")
+
+			it then uses the operator (interval[1]), the operation functions, the number arg, and the number within the interval (interval[2])
+		]]
+
+		interval    = {interval:match("([><=]=?)%D*(%d+[.]?%d*)")}
+		number      = tonumber(number)
+		interval[2] = tonumber(interval[2])
+		return defuser.operations[interval[1]](number, interval[2])
+	end
+	--[[
+		if the interval doesnt fit the first format of [operator][number] then it assumes its a [bracket][number], [number][bracket]
+
+		it breaks down the string getting the brakets ("[%[%(]" and "[%]%)]) and gets the rational numbers the same way it does above
+
+		it then decides the operator based on the type of brackets
+
+		after it returns if the number is within the interval
+	]]
+
+	interval = {interval:match("([%[%(])(%d+[.]?%d*)%D*(%d+[.]?%d*)([%]%)])")}
+	local min_operator = (interval[1] == "[" and ">=" or ">")
+	local max_operator = (interval[4] == "]" and "<=" or "<")
+	number      = tonumber(number)
+	interval[2] = tonumber(interval[2])
+	interval[3] = tonumber(interval[3])
+	return defuser.operations[min_operator](number, interval[2]) and defuser.operations[max_operator](number, interval[3])
+end
+
+function defuser.list(dictionary)
+	--[[
+		simply turns a dictionary into a table
+	]]
+
+	local array = {}
+	for index ,_ in dictionary do
+		table.insert(array, index)
+	end
+	return array
+end
+
+function defuser.strip(input, exclusions, length)
+	--[[
+		strips a string of every character not within the exclusions arg
+
+		then checks if the stripped string is the desired length and returns the stripped string if so
+
+		just realized the or nil part isnt actually needed
+	]]
+
+	local output = input:gsub(".", function(character)
+		for _, exclusion in exclusions do
+			if character:match(exclusion) then
+				return character
+			end
+		end
+		return ""
+	end)
+	return #output == length and output or nil
+end
+
+--[[
+	the following functions are the solvers for puzzles and to truly understand their functionality you're gonna need to understand how to solve them
+
+	the puzzel guide https://cdn.discordapp.com/attachments/606480627662979081/769147212742787082/Defusal_Manual.pdf?ex=66832f69&is=6681dde9&hm=c8cd1f20387f0dd53f6af110c69a6e0a7780c98238e84dcbdfbc8345da2ec510&
+]]
+
+function defuser.wires(input)
+	local wires = {
+		["1"] = {{"r", true}, {"g", true}, {"b", true}, {"y", true}, {"none"}},
+		["3"] = {{"r", false}, {"w", true}, {"b", true}},
+		["4"] = {{"g", false}, {"b", false}, {"w", false}, {"none"}}
+	}
+	input = input:gsub("%A", "")
+	local result
+	for index, wire in wires[tostring(#input)] do
+		if not result and (input:find(wire[1]) ~= nil) == wire[2] then
+			result = index
+		end
+	end
+	return result or #wires[tostring(#input)]
+end
+
+function defuser.hexadecimal(input)
+	return (input:gsub("%S%S", function(hex)
+		return string.char(tonumber(hex, 16)::number)
+	end):gsub(" ", ""))
+end
+
+function defuser.keypad(input)
+	local matches = {}
+	for match in input:gmatch("(%d%d?)") do
+		table.insert(matches, match)
+	end
+	local intervals = {
+		["none"]     = {{"+", 10}, {"-", 10}, {"+", 0}, {"*", 3}},
+		["< 10"]     = {{"+", 15}, {"+", 10}, {"*", 2}, {"*", 2}},
+		["(10, 20)"] = {{"+", 20}, {"*", 2}, {"*", 3}, {"+", 20}},
+		["(20, 80)"] = {{"+", 30}, {"*", 3}, {"-", 5}, {"+", 50}}
+	}
+	local x, y, z = 0, 0, 0
+	for index, match in matches do
+		local action
+		for interval, actions in intervals do
+			if not action and interval ~= "none" and defuser.interval(interval, match) then
+				action = actions[index]
+			end
+		end
+		action = (not action and intervals.none[index] or action)
+		x  = defuser.operations[action[1]](x, action[2])
+		y += (match / 2)
+		z  = (x - y)
+	end
+	local orders = {
+		["<= 0"]        = "TL TR BL BR",
+		["[0.5, 19.5]"] = "TL TR BR BL",
+		["[20, 49.5]"]  = "BR BL TR TL",
+		["[50, 89.5]"]  = "BL TL BR TR",
+		[">= 90"]       = "TR BL TL BR"
+	}
+	for interval, order in orders do
+		if defuser.interval(interval, z) then
+			return order
+		end
+	end
+end
+
+function defuser.binary(input)
+	local matches = {
+		"^0+$",
+		"^.1....0$",
+		"^11.....$",
+		"^0.....0$",
+		"nil",
+		"nil",
+		".*(0).*(0).*(0).*(0).*",
+		".*(1).*(1).*(1).*(1).*(1).*(1).*",
+	}
+	input = defuser.strip(input, {"[01]"}, 7)
+	for index, match in matches do
+		if input:gsub(" ", ""):match(match) then
+			return index
+		end
+	end
+	return 10
+end
+
+function defuser.mathematics(input)
+	local letters = {
+		["a"] = 1, ["b"] = 3, ["c"] = 7, ["d"] = 2, ["e"] = 4, 
+		["f"] = 5, ["g"] = 6, ["h"] = 0, ["i"] = 8, ["j"] = 9
+	}
+	input = defuser.strip(input, defuser.list(letters), 4):split("")
+	return ((letters[input[1]] * 10) + letters[input[2]]) * ((letters[input[3]] * 10) + letters[input[4]])
+end
+
+function defuser.colorcode(input)
+	
+	for index, value in input do
+		input[index] = value:gsub("[%A%s]", ""):split("")
+	end
+	local totals = {0, 0}
+	local values = {
+		["r"] = {0, 1},
+		["g"] = {0, 3},
+		["b"] = {1, 2},
+		["y"] = {2, 3},
+		["w"] = {3, 4}
+	}
+	for index, colors in input do
+		for _, color in colors do
+			totals[index] += values[color][index]
+		end
+	end
+	local total = totals[2] - totals[1]
+	return total >= 0 and total or 0
+end
+
+function defuser.timing(input)
+	local letters = {
+		["a"] = 4, ["b"] = 3, ["c"] = 7, ["d"] = 9
+	}
+	input = {input:lower():gsub("[^%d%a]",""):match("(%d%d)(%a%a)")}
+	input[1] = input[1]:split("")
+	input[1] = input[1][1] + input[1][2]
+	input[2] = input[2]:split("")
+	input[2] = letters[input[2][1]] + letters[input[2][2]]
+	local intervals = {
+		["[0, 59]"]    = "white",
+		["[60, 99]"]   = "red",
+		["[100, 199]"] = "yellow",
+		["[200, 299]"] = "green",
+		["[300, 399]"] = "blue",
+		["[400, 499]"] = "yellow",
+		["[500, 599]"] = "red",
+		[">= 600"] = "white"
+	}
+	for interval, color in intervals do
+		if defuser.interval(interval, input[1] * input[2]) then
+			return color
+		end
+	end
+end
+
+function defuser.tiles(input)
+	local colors = {
+		["r"] = 1,
+		["g"] = 9,
+		["b"] = 7,
+		["y"] = 2,
+		["p"] = 6,
+		["w"] = 5
+	}
+	input = input:lower():gsub("%A",""):match("(%a%a)"):split("")
+	return colors[input[1]] + colors[input[2]]
+end
+
+function defuser.serial(input)
+	--[[
+		unfinished function
+	
+		just a placeholder
+	]]
+
+	return input
+end
+
+function defuser.find(input)
+	--[[
+		looks through the defuser and skips the non puzzel members
+		
+		returns the found module or the string "invalid module"
+
+		input doesnt have to be a 1:1 spelling
+
+			e.g. defuser.find("hex") --> "hexadecimal"
+	]]
+
+	input = {pcall(function()
+		local skips = {"operations", "interval", "call"}
+		for module in defuser do
+			if input:gsub("%A", "") ~= "" and not table.find(skips, module) and module:match(input:lower()) then
+				return module
+			end
+		end
+	end)}
+	return (input[1] or defuser[input[2]]) and input[2] or "invalid module"
+end
+
+function defuser.call(input, ...)
+	--[[
+		uses the find function to get the module and then checks if the puzzel exist
+		
+		if it does it then pcalls the function and either returns the result or the string "invalid input"
+
+			this is to avoid having to do extra checks within the functions themselves (tldr: im lazy)
+	]]
+
+	input = defuser.find(input)
+	if not defuser[input] then return input end
+	local arguemnts = ...
+	local results = {pcall(function() return defuser[input](arguemnts) end)}
+	return (results[1] and results[2] or "invalid input")
+end
+
+return defuser
